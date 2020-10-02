d7aa4e97dc8df9d1ae94b9fe8fbf13bc77e0c115
ArminUnity <yazdianebozorg59@gmail.com>
2020-10-02 16:45:12 +0330

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+﻿const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+const prefix = '!';
+
+client.once('ready', () => {
+    console.log('your bot name is online!');
+});
+
+
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    if (command === 'صندوق') {
+        message.channel.send('شماره حساب و فلان');
+    }
+});
+
+
+client.login(process.env.BOT_TOKEN);
